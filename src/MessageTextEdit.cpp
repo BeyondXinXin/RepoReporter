df35d0636c9dded7a7cee9733dea086660f9213d
BeyondXinXin <779354187@qq.com>
2024-05-01 11:25:47 +0800

    【feat】 自定义信息栏，增加右键空白操作

---
+++ src/MessageTextEdit.cpp
@@ -0,0 +1,48 @@
+﻿#include "MessageTextEdit.h"
+
+
+MessageTextEdit::MessageTextEdit(QWidget* parent) : QTextEdit(parent)
+{
+	InitUI();
+	InitConnect();
+}
+
+void MessageTextEdit::contextMenuEvent(QContextMenuEvent* event)
+{
+	QMenu menu(this);
+
+	menu.addAction(m_CopyAction);
+	menu.addAction(m_CopyAllContentAction);
+	menu.addAction(m_EditCommentAction);
+	menu.exec(event->globalPos() - QPoint(20, 10));
+}
+
+void MessageTextEdit::InitUI()
+{
+}
+
+void MessageTextEdit::InitConnect()
+{
+	auto fun = [&](QAction *& action, const QString& iconPath, const QString& name,
+	               void (MessageTextEdit::*SlogAction)())
+		   {
+			   action = new QAction(QIcon(iconPath), name, this);
+			   connect(action, &QAction::triggered, this, SlogAction);
+		   };
+
+	fun(m_CopyAction,           "", u8"复制",     &MessageTextEdit::OnCopyAction);
+	fun(m_CopyAllContentAction, "", u8"复制所有内容", &MessageTextEdit::OnCopyAllContentAction);
+	fun(m_EditCommentAction,    "", u8"编辑注释",   &MessageTextEdit::OnEditCommentAction);
+}
+
+void MessageTextEdit::OnCopyAction()
+{
+}
+
+void MessageTextEdit::OnCopyAllContentAction()
+{
+}
+
+void MessageTextEdit::OnEditCommentAction()
+{
+}
---
+++ src/messagetextedit.h
@@ -0,0 +1,37 @@
+﻿#ifndef MESSAGETEXTEDIT_H
+#define MESSAGETEXTEDIT_H
+
+
+#include <QMenu>
+#include <QAction>
+#include <QContextMenuEvent>
+#include <QTextEdit>
+
+class MessageTextEdit : public QTextEdit {
+public:
+
+	MessageTextEdit(QWidget* parent = nullptr);
+
+protected:
+
+	void contextMenuEvent(QContextMenuEvent* event) override;
+
+private:
+
+	void InitUI();
+	void InitConnect();
+
+private slots:
+
+	void OnCopyAction();
+	void OnCopyAllContentAction();
+	void OnEditCommentAction();
+
+private:
+
+	QAction* m_CopyAction;
+	QAction* m_CopyAllContentAction;
+	QAction* m_EditCommentAction;
+};
+
+#endif // MESSAGETEXTEDIT_H
